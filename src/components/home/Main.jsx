95852a223c594ce902b5340a31f77a950dbaf8fb
arshiaTests <arshia.org79@gmail.com>
2025-03-26 02:41:38 +0330

    use props

--- src/components/home/Main.jsx
+++ src/components/home/Main.jsx
@@ -1,7 +1,12 @@
+import CourseBox from "./courseBox"
 export default function Main(){
     return(
         <div>
-            <h1>main section of home page</h1>
+            <h1>course list</h1>
+            <CourseBox title = "course number one" details = "this is about course"/>
+            <CourseBox title = "course number two" details = "javascript course course...."/>
+            <CourseBox title = "course number three" details = "react course"/>
+
 
         </div>
     )
