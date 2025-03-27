fb222337d34262a034ccbb9523cb90f07c0df220
arshiaTests <arshia.org79@gmail.com>
2025-03-27 18:12:44 +0330

    use state is usefull

--- src/components/home/Main.jsx
+++ src/components/home/Main.jsx
@@ -1,12 +1,19 @@
 import CourseBox from "./courseBox"
+import { useState } from "react"
+
 export default function Main(){
-    function eventhandler(){
-        console.log("event handler called")
+
+   const [Message , setMessage] = useState("no data here")
+
+    const eventhandler = () => {
+        setMessage("data here")
     }
+
+
     return(
         <div>
             <button onClick={eventhandler}>click me</button>
-
+            <p>{Message}</p>
             <h1>course list</h1>
             <CourseBox title = "course number one" details = "this is about course"/>
             <CourseBox title = "course number two" details = "javascript course course...."/>
