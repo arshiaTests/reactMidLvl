ef0bbec07c4c227955a066eb68429299f1ceea01
arshiaTests <arshia.org79@gmail.com>
2025-03-28 00:30:02 +0330

    list

--- src/components/home/Main.jsx
+++ src/components/home/Main.jsx
@@ -2,10 +2,13 @@ import CourseBox from "./courseBox"
 import { useState } from "react"
 
 export default function Main(){
-
+    let heading = [
+        "hello rocket",
+      " the wheater is good",
+    ]
     return(
         <div>
-    
+            {heading}
         </div>
     )
 }
