4cbad5018adffaf665e7358281f594f6af0f067c
arshiaTests <arshia.org79@gmail.com>
2025-03-27 23:19:02 +0330

    add counter

--- src/components/home/Main.jsx
+++ src/components/home/Main.jsx
@@ -3,16 +3,19 @@ import { useState } from "react"
 
 export default function Main(){
 
-   const [Message , setMessage] = useState("no data here")
+   const [Message , setMessage] = useState("no data here");
+   const [clickCount , setClickCount] = useState(0);
 
     const eventhandler = () => {
-        setMessage("data here")
+        setMessage("data here");
+        setClickCount(clickCount + 1);
     }
 
 
     return(
         <div>
             <button onClick={eventhandler}>click me</button>
+            <p>clicked {clickCount} times</p>
             <p>{Message}</p>
             <h1>course list</h1>
             <CourseBox title = "course number one" details = "this is about course"/>
