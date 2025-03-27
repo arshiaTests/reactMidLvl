9c187a5de41231bc168fcf559eec6239187d943c
arshiaTests <arshia.org79@gmail.com>
2025-03-27 17:59:17 +0330

    event handler

--- src/components/home/Main.jsx
+++ src/components/home/Main.jsx
@@ -1,7 +1,12 @@
 import CourseBox from "./courseBox"
 export default function Main(){
+    function eventhandler(){
+        console.log("event handler called")
+    }
     return(
         <div>
+            <button onClick={eventhandler}>click me</button>
+
             <h1>course list</h1>
             <CourseBox title = "course number one" details = "this is about course"/>
             <CourseBox title = "course number two" details = "javascript course course...."/>
