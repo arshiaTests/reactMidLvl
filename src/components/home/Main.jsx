c36258c664a207fb1f0b9947d512ce90d1945b46
arshiaTests <arshia.org79@gmail.com>
2025-03-28 00:06:06 +0330

    start list course

--- src/components/home/Main.jsx
+++ src/components/home/Main.jsx
@@ -3,28 +3,10 @@ import { useState } from "react"
 
 export default function Main(){
 
-   const [Message , setMessage] = useState("no data here");
-   const [clickCount , setClickCount] = useState(0);
-
-    const eventhandler = () => {
-        setMessage("data here");
-        setClickCount(clickCount + 1);
-    }
-
-
     return(
         <div>
-            <button onClick={eventhandler}>click me</button>
-            <p>clicked {clickCount} times</p>
-            <p>{Message}</p>
-            <h1>course list</h1>
-            <CourseBox title = "course number one" details = "this is about course"/>
-            <CourseBox title = "course number two" details = "javascript course course...."/>
-            <CourseBox title = "course number three" details = "react course"/>
-
-
+    
         </div>
     )
 }
 
-{/*dev tools  */}
